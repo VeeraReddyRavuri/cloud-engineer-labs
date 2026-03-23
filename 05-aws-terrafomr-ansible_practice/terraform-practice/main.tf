378a78974db44e32142ebb811665d9a84a695fc7
VeeraReddyRavuri <veerareddy.ravuri01@gmail.com>
2026-03-23 11:44:42 +0000

    Add s3 backend

--- 05-aws-terrafomr-ansible_practice/terraform-practice/main.tf
+++ 05-aws-terrafomr-ansible_practice/terraform-practice/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "YOUR_BUCKET_NAME"
+    key = "dev/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "YOUR_TABLE"
+    encrypt = true
+  }
 }
 provider "aws" {
   region = "us-east-1"
